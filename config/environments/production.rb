bb97857bdfd31784d3fff8af9e769f0ac4ef55e2
Shota <wvp360gqanukcqhg@gmail.com>
2023-08-19 19:58:33 +0900

    heroku host 設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,8 @@ Rails.application.configure do
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
 
+  config.action_mailer.default_url_options = { host: 'powerful-mesa-07530-13290fca8c13.herokuapp.com' }
+
   if ENV['RAILS_LOG_TO_STDOUT'].present?
     logger           = ActiveSupport::Logger.new($stdout)
     logger.formatter = config.log_formatter
