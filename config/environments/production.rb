8b11391b7651cafa0770c194ab92664f514d2693
Shota <wvp360gqanukcqhg@gmail.com>
2023-08-29 22:19:12 +0900

    gmail認証設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,8 +92,8 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'gmail.com',
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
+    user_name: 'wvp360gqanukcqhg@gmail.com',
+    password: 'ulihgmrjnfonukoi',
     authentication: 'plain',
     enable_starttls_auto: true
   }
