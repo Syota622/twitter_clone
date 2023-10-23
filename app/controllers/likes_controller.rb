d0588abf4cc4674116f575309c853c8b8b2c0b20
Shota <wvp360gqanukcqhg@gmail.com>
2023-10-23 22:10:39 +0900

    ActiveRecord::Base.transactionの処理を追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,16 +6,25 @@ class LikesController < ApplicationController
   def create
     @tweet = Tweet.find(params[:tweet_id])
     @like = @tweet.likes.build(user_id: current_user.id)
-    if @like.save
+  
+    ActiveRecord::Base.transaction do
+      @like.save!
+  
       # いいねの通知を作成する
       Notification.create!(
         user: @tweet.user,  # いいねされた(通知先)ユーザー
         actionable: @like   # 通知の種類、ツイートIDの二つの情報を持つ(polymorphic)
       )
+  
       # メールの送信を行う
       NotificationMailer.notify_user(@tweet.user, @like).deliver_now
     end
+    
     # 現在のページにアクセスする前にいたページにリダイレクトする。
+    redirect_to request.referer || root_path
+  
+  rescue ActiveRecord::RecordInvalid
+    # ここでエラーメッセージ等を設定しても良い
     redirect_to request.referer || root_path
   end
 
--- app/controllers/retweets_controller.rb
+++ app/controllers/retweets_controller.rb
@@ -6,16 +6,25 @@ class RetweetsController < ApplicationController
   def create
     @tweet = Tweet.find(params[:tweet_id])
     @retweet = @tweet.retweets.build(user_id: current_user.id)
-    if @retweet.save
+  
+    ActiveRecord::Base.transaction do
+      @retweet.save!
+  
       # リツイートの通知を作成する
       Notification.create!(
         user: @tweet.user, # リツイートされた(通知先)ユーザー
         actionable: @retweet # 通知の種類、ツイートIDの二つの情報を持つ(polymorphic)
       )
+  
       # メールの送信を行う
       NotificationMailer.notify_user(@tweet.user, @retweet).deliver_now
     end
+    
     # 現在のページにアクセスする前にいたページにリダイレクトする。
+    redirect_to request.referer || root_path
+  
+  rescue ActiveRecord::RecordInvalid
+    # ここでエラーメッセージ等を設定しても良い
     redirect_to request.referer || root_path
   end
 
