e9dbdda95a1f077dd74769ce37a61a42e38ee5b0
Shota <wvp360gqanukcqhg@gmail.com>
2023-10-23 22:17:03 +0900

    docker compose run --rm web bundle exec rubocop -A

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,26 +6,25 @@ class CommentsController < ApplicationController
     @tweet = Tweet.find(params[:tweet_id])
     @comment = @tweet.comments.new(comment_params)
     @comment.user = current_user
-  
+
     ActiveRecord::Base.transaction do
       @comment.save!
-  
+
       # コメントの通知を作成する
       Notification.create!(
         user: @tweet.user, # コメントされた(通知先)ユーザー
         actionable: @comment # 通知の種類、ツイートIDの二つの情報を持つ(polymorphic)
       )
-  
+
       # メールの送信を行う
       NotificationMailer.notify_user(@tweet.user, @comment).deliver_now
     end
-  
+
     # tweet_pathへリダイレクト
     redirect_to tweet_path(@tweet), notice: 'Comment was successfully created.'
-  
   rescue ActiveRecord::RecordInvalid
     render 'tweets/show'
-  end  
+  end
 
   private
 
